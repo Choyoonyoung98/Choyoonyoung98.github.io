9b4d58fc8dfe0945aaa383d6510f44f38aea4244
Choyoonyoung98 <yoon980208@gmail.com>
2022-02-06 13:50:02 +0900

    [UPDATE] 블로그 초기 셋팅 완료
    - 댓글 경로 수정
    - 메인화면 정보 수정
    - about 수정
    - 블로그 이전 글 게시

--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -9,8 +9,8 @@ function PageFooter({ author, githubUrl }) {
         &nbsp;
         <a href={githubUrl}>{author}</a>
         &nbsp;powered by
-        <a href="https://github.com/zoomKoding/zoomkoding-gatsby-blog">
-          &nbsp;zoomkoding-gatsby-blog
+        <a href="https://github.com/Choyoonyoung98/Choyoonyoung98.github.io">
+          &nbsp;Choyoonyoung98.github.io
         </a>
       </p>
     </footer>
